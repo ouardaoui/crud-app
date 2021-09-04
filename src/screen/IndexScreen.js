5054c05975eb6822a3bba4f61c2d9d88a663cae9
ouardaoui <ayoubouardaoui3@gmail.com>
2021-09-04 11:47:31 +0100

    useContext

---
+++ src/screen/IndexScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View , Text, StyleSheet } from "react-native";
+
+const IndexSceen = () => {
+    return(
+        <View>
+            <Text>index screen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+export default IndexSceen ;
